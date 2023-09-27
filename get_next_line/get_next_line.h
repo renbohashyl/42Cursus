2cba7b54e27ea639c5158aae15ecabf244380617
jbak <androtheft@outlook.kr>
2023-09-27 14:06:18 +0900

    get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbak <jbak@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 16:42:56 by jbak              #+#    #+#             */
+/*   Updated: 2022/02/18 18:04:16 by jbak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+void	check_vault(char **f_return, char **vault);
+char	*read_and_save(int fd, char *r_buf, char *vault);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+
+#endif
